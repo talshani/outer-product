838a09843f68b4bd5c88247111fcefac2c7c1c8c
I. Sinai <theblushingcrow@gmail.com>
2015-05-24 19:29:39 +0300

    copying old algorithm to new variant case

---
+++ variants/code_4.js
@@ -0,0 +1,42 @@
+function outerProduct(dimensions) {
+    if (!Array.isArray(dimensions)) throw new Error("Array expected");
+    if (!dimensions.length) return [];
+
+    var results = [];
+    var positions = [];
+    var lengths = [];
+    var skips = [];
+    var i;
+    var dimsCount = dimensions.length;
+    var skipMul = 1;
+    var dim;
+    var result;
+    var shouldSkip;
+    var len;
+
+    // prepare our states
+    for (i = 0; i < dimsCount; i++) {
+        var dimSize = dimensions[i].length;
+        lengths[i] = dimSize;
+        positions[i] = 0;
+        skips.push(skipMul);
+        skipMul *= dimSize;
+    }
+
+    // build product
+    for (i = 0; i < skipMul; i++) {
+        result = results[i] = [];
+        for (dim = 0; dim < dimsCount; dim++) {
+            shouldSkip = i % skips[dim] == 0;
+            len = lengths[dim];
+            if (shouldSkip) {
+                positions[dim] = (positions[dim] + 1) % len;
+            }
+            result.push(dimensions[dim][positions[dim]]);
+        }
+    }
+
+    return results;
+};
+
+module.exports = outerProduct;
